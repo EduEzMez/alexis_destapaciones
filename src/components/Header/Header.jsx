56db0e372457e812222db0e0d8812c49e2a46c43
Eduardo Ezequiel Gomez <edu.ezequielgomez@gmail.com>
2024-12-03 20:10:59 -0300

    ve

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import "./header-style.css"
 const Header = () => {
     const ZONAS = "AMBA Y CABA"
     const NUMERO_TEL = "11-6120-4658"
-    const LLAMAR_WAPP = "https://wa.me/1161204658?text=Hola.%20Necesito%20su%20servicio%20de%20plomeria"
+    const LLAMAR_WAPP = "https://wa.me/541161204658?text=Hola.%20Necesito%20su%20servicio%20de%20plomeria"
     const IMAGEN_PLO = "./public/plo.webp"
 
     
